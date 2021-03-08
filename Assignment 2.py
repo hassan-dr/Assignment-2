24d31f825b582209cc31a505fbfa4a4f0951da19
hassan-dr <80122912+hassan-dr@users.noreply.github.com>
2021-03-08 22:07:21 +0200

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+circumference_of_circle='c'
+circumference_of_rectangular='r'
+c=5
+r=10
+print(c+r)
+
+
+# In[16]:
+
+
+print(c)
+
+
+# In[17]:
+
+
+print(r)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
